c719392569b81c7673b296ce40ad7bff0060d677
raguggg <ragu19062002@gmail.com>
2023-12-10 16:05:00 +0530

    update

---
+++ main.py
@@ -0,0 +1,36 @@
+print("hello world")
+# write some code with 5 functions all need to do something also class
+
+def func1():
+    print("func1")
+    
+def func2():
+    print("func2")
+    
+def func3():
+    print("func3")
+
+def func4():
+    print("func4")
+    
+def func5():
+    print("func5")
+    
+class MyClass:
+    
+    def __init__(self):
+        print("MyClass")
+    
+    def func1(self):
+        print("func1")
+        
+        
+        
+if __name__ == "__main__":
+    func1()
+    func2()
+    func3()
+    func4()
+    func5()
+    myclass = MyClass()
+    myclass.func1()
